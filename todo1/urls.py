dd4d1030eab83c340743257c19db3f21672e8361
varsha <varshawagh450@gmail.com>
2024-02-19 13:29:19 +0530

    todo app final

--- todo1/urls.py
+++ todo1/urls.py
@@ -2,5 +2,15 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    #add task
     path('addTask/',views.addTask,name='addTask'),
+    #Mark as done
+    path('mark_as_done/<int:pk>',views.mark_as_done,name='mark_as_done'),
+
+    #EDIT Feature
+    path('edit_task/<int:pk>',views.edit_task,name='edit_task'),
+
+     #delete task
+     path('delete_task/<int:pk>',views.delete_task,name='delete_task'),
+
 ]
--- todo_main/views.py
+++ todo_main/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from todo1.models import Task
 def home(request):
   tasks=Task.objects.filter(is_completed=False).order_by('-updated_at')
+
+  completed_tasks=Task.objects.filter(is_completed=True)
   context = {
       'tasks':tasks,
+      'completed_tasks':completed_tasks,
     }
   return render(request,'home.html',context)
